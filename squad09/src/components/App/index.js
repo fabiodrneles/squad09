5994809f79a5088f932ae2157809bacc1d08f825
leidyolinto <suverleide.olinto@gmail.com>
2022-12-08 13:05:03 -0300

    implementacao

--- squad09/src/components/App/index.js
+++ squad09/src/components/App/index.js
@@ -1,17 +1,12 @@
 import './style.css';
-<<<<<<< HEAD
-import Seach from '../Search/index.js';
-function App() {
-  return (
-    <div className="App">
-      <Seach/>
-=======
 import React from 'react';
+import Search from '../Search/index.js';
+
 
 function App() {
   return (
     <div className="App">
->>>>>>> f49e91c01c116abc6d777dcd12cc6a6be457af84
+      <Search/>
 
     </div>
   );
