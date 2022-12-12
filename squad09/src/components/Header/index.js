6ed68109111d2e470d99d6d77603cd68acdbd12d
fabiodrneles <fabiodrneles@gmail.com>
2022-12-12 19:21:32 -0300

    refatorando header

--- squad09/src/components/Header/index.js
+++ squad09/src/components/Header/index.js
@@ -3,32 +3,36 @@ import "./style.css";
 import { IoInformationCircleSharp } from 'react-icons/io5';
 import { BsFillPersonFill } from 'react-icons/bs';
 
-function Top(){
-    return (
-        <div ClassName="Header">
+// TODO verificar divs para criar responsividade.
+// FIXME a classe botoes deve trocar o nome por um termo em inglês.
 
-            <div className="caixaMaior">
-                <div>
-                    <h2>hashtag<span>finder</span></h2>
-                </div>
 
-                <div className="botoes">
-                    <button className="btnLeft">
-                    <div className="info">
-                      <IoInformationCircleSharp/>
-                    </div>
-                      <p>Sobre</p>
-                    </button>
-                    <button className="btnRight">
-                    <div className="person">
-                      <BsFillPersonFill/>
-                    </div>
-                      <p>Login</p>
-                    </button>
-                </div>
-            </div>
+function Top() {
+  return (
+    <div ClassName="Header">
+
+      <div className="caixaMaior">
+        <div>
+          <h2>hashtag<span>finder</span></h2>
         </div>
-    );
+
+        <div className="botoes">
+          <button className="btnLeft">
+            <div className="info">
+              <IoInformationCircleSharp />
+            </div>
+            <p>Sobre</p>
+          </button>
+          <button className="btnRight">
+            <div className="person">
+              <BsFillPersonFill />
+            </div>
+            <p>Login</p>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Top;
