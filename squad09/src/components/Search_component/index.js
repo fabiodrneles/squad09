31842db23b5585d7a0511f2cbe2e41a1483ed071
leidyolinto <suverleide.olinto@gmail.com>
2022-12-12 15:06:39 -0300

    ajustes de renderização

--- squad09/src/components/Search_component/index.js
+++ squad09/src/components/Search_component/index.js
@@ -1,4 +1,5 @@
 import './style.css';
+import { ImHome} from 'react-icons';
 
 function SeachComponent() {
     return (
@@ -7,7 +8,7 @@ function SeachComponent() {
                 <div className='header' >
                     <p className='hastag'>hashtag<span className='Find'>finder</span></p>
                     <div className='buttons'>
-                        <button className='buttonHome'>HOME</button>
+                        <button className='buttonHome'> {}HOME</button>
                         <button className='buttonHeader'>SAIR</button>
                     </div>
 
@@ -18,7 +19,7 @@ function SeachComponent() {
 
                     <container className='ListContainer'>
 
-                        <div className='ListColumns'>
+                        <div className='HeaderTable'>
                             <p className='hashTagColumn'>HashTag</p>
                             <p className='dateColumn'>Data</p>
                             <p className='hourColumn'>Hora</p>
@@ -29,6 +30,7 @@ function SeachComponent() {
                             <li className='hourColumns'>09:35</li>
                         </ul>
                         <hr className='Line'></hr>
+                        
                         <ul className='ListHastags'>
                             <li className='ListMarcadores'>#hastagname</li>
                             <li className='dateColumns'>25/02</li>
--- squad09/src/views/Search/index.js
+++ squad09/src/views/Search/index.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Footer from "../../components/Footer/Footer";
 
 import SeachComponent from "../../components/Search_component";
 function Search(){
   return(
     <>
     <SeachComponent/>
+    <Footer/>
     </>
   )
 }
