37aac10d310b92cfd679795ab1b3262350fe2e4c
fabiodrneles <fabiodrneles@gmail.com>
2022-12-15 21:32:37 -0300

    editando a views home

--- squad09/src/views/Home/index.js
+++ squad09/src/views/Home/index.js
@@ -1,13 +1,37 @@
 import Context from "../../components/context";
 import Footer from "../../components/Footer";
+//import IconSearch from '../../assets/icon-search.svg'; => imagem da lupa
+import IconLogin from '../../assets/icon-user-alt.svg'
+import IconInfo from '../../assets/icon-info-circle.svg';
+import Header from "../../components/Header";
 import "./style.css";
 
 
 function Home() {
+    const buttonStyles = [
+        {
+            title: "SOBRE",
+            icon: IconInfo,
+            route: "/about",
+            textColor: "#0A1744",
+            backgroundColor: "#72EFDB",
+        },
+        {
+            title: "LOGIN",
+            icon: IconLogin,
+            route: "/login",
+            textColor: "#FFF",
+            backgroundColor: "#0B1A49",
+        },
+        
+    ]
+
+
+
     return (
         <>
 
-
+            <Header buttons={buttonStyles}/>
             <Context />
             <Footer />
 
