5b3e7fda7bf7032d086d52cade07c7e15fcb0942
fabiodrneles <fabiodrneles@gmail.com>
2022-12-16 20:39:15 -0300

    todo inserindo

--- squad09/src/views/Home/index.js
+++ squad09/src/views/Home/index.js
@@ -4,6 +4,10 @@ import Footer from "../../components/Footer";
 import IconLogin from '../../assets/icon-user-alt.svg'
 import IconInfo from '../../assets/icon-info-circle.svg';
 import Header from "../../components/Header";
+// IMPORT DA IMAGEM 
+// TODO FAZER A TROCA DE IMAGEM NO FUNDO DA PAGINA
+import IconIlustration from '../../assets/about-ilustration.svg';
+
 import "./style.css";
 
 
@@ -23,7 +27,7 @@ function Home() {
             textColor: "#FFF",
             backgroundColor: "#1E3E7B",
         },
-        
+
     ]
 
 
@@ -31,8 +35,33 @@ function Home() {
     return (
         <>
 
-            <Header buttons={buttonStyles}/>
+            <Header buttons={buttonStyles} />
             {/*<Context /> */}
+//TODO EDITAR A ESCRITA
+//TODO EDITAR AS classNaME DE ACORDO COM A VIEWS lOGIN
+            <div className="about">
+                <div className="aboutProject">
+                    <div className="aboutProjectTitle">
+                        <h1>Sobre o projeto</h1>
+                    </div>
+                    <div className="projectText">
+                        <p>
+                            Certamente, você já encontrou a palavra engajamento em diferentes contextos,
+                            e nós alunos da NewTab Academy estamos engajados em te proporcionar ferramentas
+                            que otimizem suas buscas de maneira assertiva.
+                        </p>
+                        <p>
+                            Aqui no hashtagfinder, você acessa de forma rápida e simples as hashs mais comentadas do Twitter,
+                            garantindo assim maior interação em suas postagens, além de monitorar o crescimento de suas próprias
+                            hashes personalizadas e te deixar por dentro das maiores novidades, e tudo isso de forma gratuita.
+                        </p>
+                    </div>
+                </div>
+                <div className="aboutIlustration">
+                    <img src={IconIlustration} alt="ilustração" className="IlustrationImage" />
+                </div>
+            </div>
+
             <Footer />
 
         </>
