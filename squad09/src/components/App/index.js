86f42dbbe6171110468fee505cc7e0b9c98553e7
bimaggi <bmg.olivier@gmail.com>
2022-12-16 20:51:40 -0300

    import da fonte e alteração dos nomes dos arquivos dentro da pasta App

--- squad09/src/components/App/index.js
+++ squad09/src/components/App/index.js
@@ -6,17 +6,14 @@ import Login from '../../views/Login'
 import About from '../../views/About'
 
 function App() {
-    return (
-        <div className="App">
-
+  return (
+    <div className="App">
       <Routes>
-
         <Route path="/" element={ <Home /> }/>
         <Route path="/login" element={ <Login/> }/>
         <Route path="/about" element={ <About/> }/>
         <Route path="/search" element={ <Search/> }/>
       </Routes>
-
     </div>
   );
 }
