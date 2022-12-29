f7c9c1486ab90be0b7b86b7b3bb861a646a79914
leidyolinto <suverleide.olinto@gmail.com>
2022-12-29 01:30:03 -0300

    atualizando get da api

--- squad09/src/index.js
+++ squad09/src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import App from './components/App';
-import { BrowserRouter as Router } from "react-router-dom"
-import ReactDOM from 'react-dom'
+import { BrowserRouter as Router } from "react-router-dom";
+//import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom/client";
 
-ReactDOM.render(
+// ReactDOM.render(
+//   <Router>
+//     <App />
+//   </Router>,
+//   document.getElementById('root')
+// )
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <Router>
-    <App/>
+    <App />
   </Router>,
-  document.getElementById('root')
-)
+);
--- squad09/src/views/Search/index.js
+++ squad09/src/views/Search/index.js
@@ -4,8 +4,13 @@ import HomeIcon from '../../assets/icon-home.svg';
 import OffIcon from '../../assets/icon-power-off.svg'
 import Header from "../../components/Header";
 import Footer from '../../components/Footer'
+import { useEffect, useState } from 'react';
+
 
 function Seach() {
+    const [lista, setLista] = useState([])
+    const [currentPage, setCurrentPage] = useState(0);
+
     const buttonStyles = [
         {
             title: "HOME",
@@ -22,6 +27,69 @@ function Seach() {
             backgroundColor: "#1E3E7B",
         },
     ]
+
+    useEffect(() => {
+    fetch("https://api.airtable.com/v0/app6wQWfM6eJngkD4/buscas?filterByFormula=({Squad} = '09-22')", {
+        headers: {
+            Authorization: "Bearer keyz8BAZKCTGY5dB1"
+        }
+    })
+      .then(response => response.json())
+      .then(result => console.log(result))
+      .catch(error => console.log('error', error));
+
+      fetch('https://api.airtable.com/v0/app6wQWfM6eJngkD4/Equipe?filterByFormula=Find(%2209-22%22%2C+Squad)', 
+      { 
+        method: "GET", 
+        headers: { 
+          "Authorization": `Bearer keyz8BAZKCTGY5dB1`, 
+        } 
+      })
+      .then(function (res) { return res.json(); })
+      .then(function (data) { 
+       console.log("Deu certo")
+        setCurrentPage([
+          {
+            //   _hashtag: data.filds.Hashtag,
+            //   get hashtag() {
+            //       return this._hashtag;
+            //   },
+            //   set hashtag(value) {
+            //       this._hashtag = value;
+            //   },
+            // _data: data.fields.Data,
+            // get Data(){
+            //     return this._data;
+            // },
+
+            // _hora: data.fields.Hora,
+            // get Hora(){
+            //     return this._hora;
+            // }
+
+
+          },
+         
+        ])
+      })
+    },[])
+//FORMATAR A DATA
+    function formataData(data) {
+        let novaData = new Intl.DateTimeFormat("pt-br", {
+            day: "2-digit",
+            month: "2-digit",
+        }).format(data);
+        return novaData;
+    }
+//FORMATAR HORA
+    function formataHora(hora) {
+        let novaHora = new Intl.DateTimeFormat("pt-br", {
+            hour: "numeric",
+            minute: "numeric",
+        }).format(hora);
+        return novaHora;
+    }
+
     return (
         <header className='pageSearch'>
             <div className='ListMain ,mobile'>
@@ -30,77 +98,28 @@ function Seach() {
                 <div className='ListTitle'>Buscas Realizadas</div>
                 <div className='ListTable'>
 
-                    <container className='ListContainer'>
+                    <div id='ListContainer'>
 
                         <div className='HeaderTable'>
                             <p className='hashTagColumn'>HashTag</p>
                             <p className='dateColumn'>Data</p>
                             <p className='hourColumn'>Hora</p>
                         </div>
-                        <ul className='ListHastags'>
-                            <li className='ListMarcadores'>#hastagname</li>
-                            <li className='dateColumns'>25/02</li>
-                            <li className='hourColumns'>09:35</li>
-                        </ul>
-                        <hr className='Line'></hr>
+                            {lista.map((user, i) => (
+                                
+                                <ul className='ListHastags' key={i}>
+                                    <li className='ListMarcadores'>{user.fields.Hashtag}</li>
+                                    <li className='dateColumns'>{formataData(user.fields.Data)}</li>
+                                    <li className='hourColumns'>{formataHora(user.fields.hora)}</li>
+                                    <hr className='Line'></hr>
+                                </ul>
+                                    
+                            ))}
+                    </div>
 
-                        <ul className='ListHastags'>
-                            <li className='ListMarcadores'>#hastagname</li>
-                            <li className='dateColumns'>25/02</li>
-                            <li className='hourColumns'>09:35</li>
-                        </ul>
-                        <hr className='Line'></hr>
-                        <ul className='ListHastags'>
-                            <li className='ListMarcadores'>#hastagname</li>
-                            <li className='dateColumns'>25/02</li>
-                            <li className='hourColumns'>09:35</li>
-                        </ul>
-                        <hr className='Line'></hr>
-                        <ul className='ListHastags'>
-                            <li>#hastagname</li>
-                            <li className='dateColumns'>25/02</li>
-                            <li className='hourColumns'>09:35</li>
-                        </ul>
-                        <hr className='Line'></hr>
-                        <ul className='ListHastags'>
-                            <li>#hastagname</li>
-                            <li className='dateColumns'>25/02</li>
-                            <li className='hourColumns'>09:35</li>
-                        </ul>
-                        <hr className='Line'></hr>
-                        <ul className='ListHastags'>
-                            <li>#hastagname</li>
-                            <li className='dateColumns'>25/02</li>
-                            <li className='hourColumns'>09:35</li>
-                        </ul>
-                        <hr className='Line'></hr>
-                        <ul className='ListHastags'>
-                            <li>#hastagname</li>
-                            <li className='dateColumns'>25/02</li>
-                            <li className='hourColumns'>09:35</li>
-                        </ul>
-                        <hr className='Line'></hr>
-                        <ul className='ListHastags'>
-                            <li>#hastagname</li>
-                            <li className='dateColumns'>25/02</li>
-                            <li className='hourColumns'>09:35</li>
-                        </ul>
-                        <hr className='Line'></hr>
-                        <ul className='ListHastags'>
-                            <li className='ListMarcadores'>#hastagname</li>
-                            <li className='dateColumns'>25/02</li>
-                            <li className='hourColumns'>09:35</li>
-                        </ul>
-                        <hr className='Line'></hr>
-                        <ul className='ListHastags'>
-                            <li className='ListMarcadores'>#hastagname</li>
-                            <li className='dateColumns'>25/02</li>
-                            <li className='hourColumns'>09:35</li>
-                        </ul>
-                    </container>
                 </div>
             </div>
-            <Footer/>
+            <Footer />
         </header>
     );
 };
